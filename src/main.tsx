c97b342983afba0f2e488b8a3530bf8c1c29cafb
PrabinGautam <prabeen122@gmail.com>
2024-02-06 21:11:50 +0545

    init: initlaize shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './styles/index.css';
-import CharacterHeroWithSearch from './components/hero/CharacterHeroWithSearch';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./styles/index.css";
+import CharacterHeroWithSearch from "@/components/hero/CharacterHeroWithSearch";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <CharacterHeroWithSearch />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
