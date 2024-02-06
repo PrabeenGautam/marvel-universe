e59e40ad29ea74791ea3479539224b998a35c53b
PrabinGautam <prabeen122@gmail.com>
2024-02-06 22:01:43 +0545

    feat: added search bar to landing home container

--- src/components/hero/CharacterHeroWithSearch.tsx
+++ src/components/hero/CharacterHeroWithSearch.tsx
@@ -1,19 +1,24 @@
-// Welcome field for the character page with a search bar
+import SearchBar from "../ui/SearchBar";
+
 function CharacterHeroWithSearch() {
   return (
-    <div className="hero-container flex-center h-100 relative bg-black px-4 text-white">
+    <div className="hero-container flex-center relative h-100 bg-black px-4 text-white">
       {/* // Background wrapper*/}
-      <div className="hero-wrapper before:bg-hero-pattern z-[1] before:absolute before:inset-0 before:bg-cover before:bg-no-repeat  after:absolute after:inset-0 after:bg-black/50"></div>
+      <div className="hero-wrapper z-[1] before:absolute before:inset-0 before:bg-hero-pattern before:bg-cover before:bg-center  before:bg-no-repeat after:absolute after:inset-0 after:bg-black/50"></div>
 
       {/* // Content */}
-      <div className="hero-content z-[2] space-y-4 text-center">
-        <h1 className="text-2xl font-bold uppercase transition-all lg:text-4xl">
+      <div className="hero-content z-[2] space-y-4 sm:text-center">
+        <h1 className="font-bold uppercase transition-all sm:text-2xl lg:text-4xl">
           Marvel Universe
         </h1>
         <span className="block leading-tight tracking-wide">
           Dive into the Marvel Universe's thrilling tales of heroes and
           villains!
         </span>
+
+        <div className="!mt-8 basis-96">
+          <SearchBar />
+        </div>
       </div>
     </div>
   );
---
+++ src/components/ui/SearchBar.tsx
@@ -0,0 +1,19 @@
+import { Search } from "lucide-react";
+
+function SearchBar() {
+  return (
+    <form className="flex w-full select-none rounded bg-white px-2 py-1.5">
+      <Search className="stroke-black" />
+
+      <input
+        type="text"
+        className="mx-2.5 w-full border-none bg-inherit text-sm text-black outline-none"
+        placeholder="Search for a character..."
+        name="query"
+      />
+      <input type="submit" hidden />
+    </form>
+  );
+}
+
+export default SearchBar;
