a9f1b8524db0dde562ebd879dae9d0fc8dd19c88
PrabinGautam <prabeen122@gmail.com>
2024-02-07 17:48:49 +0545

    fix: make character hero page responsive

--- src/components/hero/CharacterHero.tsx
+++ src/components/hero/CharacterHero.tsx
@@ -3,23 +3,23 @@ import BackgroundHero from "./BackgroundHero";
 
 function CharacterHero() {
   return (
-    <div className="group relative h-96">
+    <div className="group relative sm:h-96">
       {/* // Background wrapper*/}
       <BackgroundHero opacity={1} scale={3} />
 
       {/* // Content */}
-      <div className="z-10 mx-auto flex h-full max-w-6xl items-center gap-16">
-        <div className="img-wrapper relative h-full overflow-hidden grayscale hover:bg-transparent hover:grayscale-0">
+      <div className="z-10 mx-auto flex h-full max-w-6xl flex-col items-center sm:flex-row sm:gap-8 lg:gap-16">
+        <div className="img-wrapper relative h-full overflow-hidden grayscale transition-all group-hover:grayscale-0">
           <img
             src="https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
             alt="character"
-            className="h-full object-cover transition-transform duration-300 ease-in-out hover:mix-blend-normal group-hover:scale-105"
+            className="h-full object-cover transition-all group-hover:scale-105"
           />
         </div>
 
         {/* Character Details  */}
-        <div className="space-y-4 text-white">
-          <h1 className="font-bold uppercase transition-all sm:text-2xl lg:text-6xl">
+        <div className="space-y-4 p-6 text-white sm:p-0">
+          <h1 className="text-2xl font-bold uppercase transition-all lg:text-6xl">
             LOKI
           </h1>
 
