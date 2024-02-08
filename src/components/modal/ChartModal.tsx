fb6af3f488bb5e470272b13afb9e6943b2637d71
PrabinGautam <prabeen122@gmail.com>
2024-02-08 19:09:36 +0545

    fix: changed alert dialog to dialog only

--- src/components/modal/ChartModal.tsx
+++ src/components/modal/ChartModal.tsx
@@ -1,24 +1,27 @@
 import {
-  AlertDialog,
-  AlertDialogContent,
-  AlertDialogHeader,
-  AlertDialogTrigger,
-} from "@/components/ui/alert-dialog";
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import CharacterComicChart from "../chart/ComicAvailable";
+import { useState } from "react";
 
 export function CharacterChart() {
+  const [open, setOpen] = useState(false);
+
   return (
-    <AlertDialog open>
-      <AlertDialogTrigger asChild>
+    <Dialog open={open} onOpenChange={setOpen}>
+      <DialogTrigger asChild>
         <div className="flex cursor-pointer items-center gap-4 px-2 py-1.5 font-roboto text-sm font-medium uppercase text-gray-300 hover:text-primary">
           View Graphically
         </div>
-      </AlertDialogTrigger>
-      <AlertDialogContent className="border-none px-0 md:p-6">
-        <AlertDialogHeader>
+      </DialogTrigger>
+      <DialogContent className="border-none px-0 md:p-6">
+        <DialogHeader>
           <CharacterComicChart />
-        </AlertDialogHeader>
-      </AlertDialogContent>
-    </AlertDialog>
+        </DialogHeader>
+      </DialogContent>
+    </Dialog>
   );
 }
