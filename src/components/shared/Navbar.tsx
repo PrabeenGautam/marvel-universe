f4391569eeb2965ff620134f67b2a35d423d12b4
PrabinGautam <prabeen122@gmail.com>
2024-02-09 18:08:40 +0545

    Update Navbar component

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -3,11 +3,19 @@ import Image from "./Image";
 
 function Navbar() {
   return (
-    <nav className="sticky top-0 z-30 bg-black">
-      <div className="relative mx-auto w-full max-w-screen-xl px-4 py-5 text-white">
+    <nav className="sticky top-0 mt-4 bg-black/50">
+      <div className="mx-auto flex w-full max-w-screen-xl flex-wrap items-center justify-between gap-8 p-4 md:py-8">
         <Link to="/">
           <Image src="/static/logo.jpg" className="h-10" alt="Marvel Logo" />
         </Link>
+
+        <ul className="flex items-center text-sm font-medium text-gray-500 dark:text-gray-400">
+          <li>
+            <Link to="/characters" className="me-4 hover:underline md:me-6">
+              Explore Characters
+            </Link>
+          </li>
+        </ul>
       </div>
     </nav>
   );
