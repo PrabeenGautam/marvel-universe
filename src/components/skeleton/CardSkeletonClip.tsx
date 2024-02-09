9aacc87d16d63437ce85f23d1ea5281194883b5b
PrabinGautam <prabeen122@gmail.com>
2024-02-09 17:34:22 +0545

    Refactor CardSkeletonClip component

--- src/components/skeleton/CardSkeletonClip.tsx
+++ src/components/skeleton/CardSkeletonClip.tsx
@@ -3,10 +3,21 @@ import { Skeleton } from "../ui/skeleton";
 function CardSkeletonClip() {
   return (
     <div className="grid gap-x-2 gap-y-6 xs:grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5">
-      {Array.from({ length: 10 }).map((_, index) => (
-        <Skeleton key={index} className="clip-border aspect-2/3" />
-      ))}
+      <CardSkeletonBase />
     </div>
+  );
+}
+
+/**
+ * Base skeleton for card component with aspect ratio 2/3
+ */
+export function CardSkeletonBase() {
+  return (
+    <>
+      {Array.from({ length: 10 }).map((_, index) => (
+        <Skeleton key={index} className="aspect-2/3" />
+      ))}
+    </>
   );
 }
 
